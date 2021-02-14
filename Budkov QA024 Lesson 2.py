a0f4c050440e39625215fc40e69215b2ca18370c
Anton Budkov <76530161+Tonykane923@users.noreply.github.com>
2021-02-14 23:05:54 +0300

    Add files via upload

---
+++ Budkov QA024 Lesson 2.py
@@ -0,0 +1,8 @@
+print("Nothing\nWill work\nUnless you do")
+print('==================================')
+print("Nothing will work")
+print("Unless you do")
+
+print("=======================================")
+
+print("\"Anyone\nwho stops learning\nis old,\nwhether\nat twenty or eighty\".\nHenry Ford")
