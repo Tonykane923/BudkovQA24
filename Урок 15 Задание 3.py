c9749dbd1ddce504333e0387eb8464c3bf31f7b0
Anton Budkov <76530161+Tonykane923@users.noreply.github.com>
2021-03-29 21:13:41 +0300

    Add files via upload

---
+++ Урок 15 Задание 3.py
@@ -0,0 +1,20 @@
+# пишем функцию
+def Complex(n):
+    if n <= 1 or n <= 3:
+        return False
+    if n % 2 == 0 or n % 3 == 0:
+        return True
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return True
+        i += 6
+    return False
+
+
+# пример вызова
+x = int(input('Введите число: '))
+if Complex(x):
+    print('Число {} - составное'.format(x))
+else:
+    print('Число {} - простое'.format(x))
